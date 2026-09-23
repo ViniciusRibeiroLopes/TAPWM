abbc708ca5b189135a44211d32b253a14bfc559d
ViniciusRibeiroLopes <vi1719nicius@gmail.com>
2026-09-23 14:09:55 -0300

    refactor: Corrigido erro de path

--- Atividade22/app/routes/editar_professores.js
+++ Atividade22/app/routes/editar_professores.js
@@ -3,11 +3,11 @@ module.exports = function (app) {
         async function getProfessorPorId() {
             try {
                 let id_professor = req.query.id;
-                let connection = application.config.dbConnection;
+                let connection = app.config.dbConnection;
                 const pool = await connection();
-                let professoresModel = application.models.professormodel;
+                let professoresModel = app.models.professormodel;
                 professoresModel.getProfessorPorId(id_professor, pool, function (error, results) {
-                    res.render('admin/editar_professor', { profs: results.recordset });
+                    res.render('admin/editar_professores', { profs: results.recordset });
                 });
             } catch (err) {
                 console.log(err)
@@ -19,13 +19,10 @@ module.exports = function (app) {
         async function editarProfessor() {
             try {
                 let professor = req.body;
-                let connection = application.config.dbConnection;
+                let connection = app.config.dbConnection;
                 const pool = await connection();
-                let professoresModel = application.models.professormodel;
-                // //usando uma funcao de callback e informar quem devemos salvar, no caso professor
+                let professoresModel = app.models.professormodel;
                 professoresModel.editarProfessor(professor, pool, (error, results) => {
-                    // após inserir redireciona o navegador para outra página
-                    // se der erro na inclusao criar um erro 500 --> nao sabe o que significa
                     if (error) {
                         console.log('Erro ao inserir no banco:' + error);
                         res.status(500).send(error);
